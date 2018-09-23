d7e7c0b47d55835f88fa17f61226a7ed8ac2cd7c
Errin Mixon <errinmixon@gmail.com>
2018-09-22 22:45:44 -0700

    added files and ran python script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hw2redo
+must run either build.sh or build.py to
+generate page
---
+++ build.py
@@ -0,0 +1,89 @@
+about_top_template = open('templates/about_top.html').read()
+about_content = open('content/about.html').read()
+about_html = about_top_template + about_content
+open('about.html', 'w+').write(about_html)
+
+contact_top_template = open('templates/contact_top.html').read()
+contact_content = open('content/contact.html').read()
+contact_html = contact_top_template + contact_content
+open('contact.html', 'w+').write(contact_html)
+
+stories_top_template = open('templates/stories_top.html').read()
+stories_content = open('content/stories.html').read()
+stories_html = stories_top_template + stories_content
+open('stories.html', 'w+').write(stories_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+belgium_content = open('content/belgium.html').read()
+belgium_html = blog_top_template + belgium_content
+open('belgium.html', 'w+').write(belgium_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+bhutan_content = open('content/bhutan.html').read()
+bhutan_html = blog_top_template + bhutan_content
+open('bhutan.html', 'w+').write(bhutan_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+egypt_content = open('content/egypt.html').read()
+egypt_html = blog_top_template + egypt_content
+open('egypt.html', 'w+').write(egypt_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+india_content = open('content/india.html').read()
+india_html = blog_top_template + india_content
+open('india.html', 'w+').write(india_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+ireland_content = open('content/ireland.html').read()
+ireland_html = blog_top_template + ireland_content
+open('ireland.html', 'w+').write(ireland_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+slovenia_content = open('content/slovenia.html').read()
+slovenia_html = blog_top_template + slovenia_content
+open('slovenia.html', 'w+').write(slovenia_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+spain_content = open('content/spain.html').read()
+spain_html = blog_top_template + spain_content
+open('spain.html', 'w+').write(spain_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+uzbekistan_content = open('content/uzbekistan.html').read()
+uzbekistan_html = blog_top_template + uzbekistan_content
+open('uzbekistan.html', 'w+').write(uzbekistan_html)
+
+blog_top_template = open('templates/blog_top.html').read()
+vietnam_content = open('content/vietnam.html').read()
+vietnam_html = blog_top_template + vietnam_content
+open('vietnam.html', 'w+').write(vietnam_html)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
